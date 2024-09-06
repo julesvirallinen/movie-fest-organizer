5f4ede230b9899430add88705c80c1ccea8cbc31
julesvirallinen <julius@uusinarkaus.com>
2024-09-06 11:36:12 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/app/layout.tsx
@@ -0,0 +1,41 @@
+import { GeistSans } from 'geist/font/sans'
+import type { Metadata } from 'next'
+import './globals.css'
+
+import { AuthProvider, Navbar, ThemeProvider } from '@/components/layout'
+
+import { AtomicState } from 'atomic-state'
+import { FetchConfig } from 'http-react'
+
+export const metadata: Metadata = {
+  title: 'Home',
+  description: 'Home page '
+}
+
+export default function MainLayout({ children }) {
+  return (
+    <html suppressHydrationWarning>
+      <head>
+        <title>Next.js starter</title>
+        <meta name='description' content='A Starter with Next.js' />
+      </head>
+
+      <body className={GeistSans.className}>
+        <ThemeProvider attribute='class' defaultTheme='system'>
+          <main className='min-h-screen'>
+            <AuthProvider>
+              <AtomicState>
+                <FetchConfig baseUrl='/api'>
+                  <Navbar />
+                  <div className='max-w-screen-2xl mx-auto py-8 px-6 md:px-8'>
+                    {children}
+                  </div>
+                </FetchConfig>
+              </AtomicState>
+            </AuthProvider>
+          </main>
+        </ThemeProvider>
+      </body>
+    </html>
+  )
+}
---
+++ src/components/post/PostForm.tsx
@@ -0,0 +1,107 @@
+'use client'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useMutation } from 'http-react'
+import { AlertCircle, Loader2 } from 'lucide-react'
+import { useRouter } from 'next/navigation'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+import { Alert, AlertTitle } from '@/components/ui/alert'
+import { Button } from '@/components/ui/button'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { Textarea } from '@/components/ui/textarea'
+
+import { createPost } from '@/actions/post'
+import { postSchema } from '@/schemas'
+
+type FormSchema = z.infer<typeof postSchema>
+
+export default function PostForm() {
+  const router = useRouter()
+
+  const form = useForm<FormSchema>({
+    resolver: zodResolver(postSchema),
+    defaultValues: {
+      title: '',
+      content: ''
+    }
+  })
+
+  // To learn how to use the `useMutation` hook with server actions
+  // visit https://httpr.vercel.app/docs/server_actions#server-mutations
+
+  const { refresh, loading, error } = useMutation(createPost, {
+    params: form.getValues(),
+    onResolve() {
+      router.replace('/posts')
+    }
+  })
+
+  const onSubmit = form.handleSubmit(refresh)
+
+  return (
+    <Form {...form}>
+      {error && (
+        <Alert className='mb-4' variant='destructive'>
+          <AlertCircle className='h-4 w-4' />
+          <AlertTitle>An error ocurred</AlertTitle>
+        </Alert>
+      )}
+
+      <form onSubmit={onSubmit} className='w-full space-y-6'>
+        <FormField
+          control={form.control}
+          name='title'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Title</FormLabel>
+              <FormControl>
+                <Input placeholder='Post title' {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name='content'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Description</FormLabel>
+              <FormControl>
+                <Textarea
+                  className='resize-none'
+                  placeholder='Post description'
+                  {...field}
+                  onChange={e => {
+                    const heightOffset = 3
+                    e.currentTarget.style.height = 'auto'
+                    e.currentTarget.style.height =
+                      e.currentTarget.scrollHeight + heightOffset + 'px'
+
+                    field.onChange(e)
+                  }}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <div className='flex justify-end'>
+          <Button disabled={loading} type='submit'>
+            {loading && <Loader2 className='mr-2 h-4 w-4 animate-spin' />}
+            Create Post
+          </Button>
+        </div>
+      </form>
+    </Form>
+  )
+}
